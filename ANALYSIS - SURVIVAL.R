b336b10b7fc84621a184f2141c95c923e19b3156
LizzieAugarde <lizzie994@googlemail.com>
2024-02-14 17:25:06 +0000

    Changed approach to survival analysis to be based on diag date not survey year

--- ANALYSIS - SURVIVAL.R
+++ ANALYSIS - SURVIVAL.R
@@ -5,11 +5,16 @@
 
 #Created January 2024 by Lizzie Augarde 
 #Change log:
+#14/02/2024 redoing survival datasets to use all patients with 1 and 5 years post-diag
+#available in COSD
 ############################################################################# 
+
+library(xlsx)
 
 ########### 1yr survival dataset for analysis ###########
 surv_1yr_data <- resp_data %>%
-  select(-starts_with("Q"))
+  select(-starts_with("Q")) %>%
+  filter((DIAGNOSISDATEBEST + 365) < "2022-01-01") #filtering to just those with at least 1 year 'available' from diag date
 
 #multiple responses 
 surv_1yr_data_dups_check <- surv_1yr_data  %>%
@@ -31,11 +36,13 @@ unique(surv_1yr_data_dups_check$deathdate_match) #no mismatching death dates
 
 #ranking
 surv_1yr_data_dups_rank <- rank_multiple_responses(surv_1yr_data_dups_check, "PATIENTID", "datayear")
+
 surv_1yr_data_dups_rank <- surv_1yr_data_dups_rank %>% 
   filter(rank == 1) %>%
   select(-rank, -deathdate_match)
 
 surv_1yr_data <- rbind(surv_1yr_data_single_response, surv_1yr_data_dups_rank) 
+
 #check 
 length(unique(surv_1yr_data$PATIENTID))
 
@@ -71,15 +78,42 @@ write.xlsx(as.data.frame(surv_1yr_language),
            sheetName = "1yr survival language", row.names = FALSE, append = TRUE)
 
 
-########### 5yr survival dataset for analysis ########### - CANNOT USE THIS UNTIL WE HAVE 2022 DATA
+########### 5yr survival dataset for analysis ###########
 surv_5yr_data <- resp_data %>%
-  filter(datayear == 2017) %>%
-  select(-starts_with("Q"))
+  select(-starts_with("Q")) %>%
+  filter((DIAGNOSISDATEBEST + 1825) < "2022-01-01") #filtering to just those with at least 5 years 'available' from diag date
+
+#multiple responses 
+surv_5yr_data_dups_check <- surv_5yr_data  %>%
+  group_by(PATIENTID) %>% 
+  filter(n() > 1) 
+
+surv_5yr_data_single_response <- surv_5yr_data %>%
+  group_by(PATIENTID) %>% 
+  filter(n() == 1) 
+
+source("multiple_responses_functions.R")
+
+#checking for matching death dates in multiple responses
+surv_5yr_data_dups_check <- surv_5yr_data_dups_check %>%
+  group_by(PATIENTID) %>%
+  mutate(deathdate_match = n_distinct(DEATHDATEBEST))
+
+unique(surv_5yr_data_dups_check$deathdate_match) #no mismatching death dates
+
+#ranking
+surv_5yr_data_dups_rank <- rank_multiple_responses(surv_5yr_data_dups_check, "PATIENTID", "datayear")
+
+surv_5yr_data_dups_rank <- surv_5yr_data_dups_rank %>% 
+  filter(rank == 1) %>%
+  select(-rank, -deathdate_match)
+
+surv_5yr_data <- rbind(surv_5yr_data_single_response, surv_5yr_data_dups_rank) 
 
 #check 
 length(unique(surv_5yr_data$PATIENTID))
 
-write.csv(rtd_data, "N:/INFO/_LIVE/NCIN/Macmillan_Partnership/Linked CPES-registry analysis/Data/RTD dataset for analysis.csv")
+write.csv(surv_5yr_data, "N:/INFO/_LIVE/NCIN/Macmillan_Partnership/Linked CPES-registry analysis/Data/Survival 5yr dataset for analysis.csv")
 
 
 ########### Overall 5yr survival descriptive stats ########### 
