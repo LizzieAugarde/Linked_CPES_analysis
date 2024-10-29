aa14550ebed54919eb56ee5f20f5ec47f8c71dcb
LizzieAugarde <lizzie994@googlemail.com>
2024-10-29 16:34:02 +0000

    Updated CPES 2022 table names following correction of the linkage

--- DATA EXTRACTION AND PREP.R
+++ DATA EXTRACTION AND PREP.R
@@ -3,12 +3,6 @@
 #Extract of linked CPES data
 
 #Created December 2023 by Lizzie Augarde 
-#Change log:
-#14/12/2023 added analysis of patients responding in multiple years
-#05/01/2024 moved multiple responses analysis to separate exploratory script
-#and added in extract of IMD data
-#10/06/2024 added in 2022 respondent data, RTD for 2019 respondents, and moved 
-#data prep steps to this script. Added save of resp_data dataframe for quarto
 ############################################################################# 
 
 library(tidyverse)
@@ -18,7 +12,7 @@ casref01 <- NDRSAfunctions::createConnection()
 
 #data extract - 2022
 resp2022_query <- "SELECT *
-                  FROM CPES.NC_2022_RESPONDENTS@casref01 cpes, CPES.NC_2022_LINKAGE_RES_CAS2403@casref01 ln, AV2021.AT_TUMOUR_ENGLAND@casref01 at
+                  FROM CPES.NC_2022_RESPONDENTS@casref01 cpes, CPES.NC_2022_LINKAGE_RES_CAS2407@casref01 ln, AV2021.AT_TUMOUR_ENGLAND@casref01 at
                   WHERE cpes.cpes_prn = ln.cpes_prn
                   AND ln.avtum_patientid = at.patientid
                   AND ln.avtum_tumourid = at.tumourid
@@ -78,11 +72,11 @@ resp2019$datayear <- 2019
 resp2018$datayear <- 2018
 resp2017$datayear <- 2017
 
-resp2022 <- resp2022 %>% mutate_all(as.character)
-resp2021 <- resp2021 %>% mutate_all(as.character)
-resp2019 <- resp2019 %>% mutate_all(as.character)
-resp2018 <- resp2018 %>% mutate_all(as.character)
-resp2017 <- resp2017 %>% mutate_all(as.character)
+resp2022 <- resp2022 |> mutate_all(as.character)
+resp2021 <- resp2021 |> mutate_all(as.character)
+resp2019 <- resp2019 |> mutate_all(as.character)
+resp2018 <- resp2018 |> mutate_all(as.character)
+resp2017 <- resp2017 |> mutate_all(as.character)
 
 resp_data <- bind_rows(resp2022, resp2021, resp2019, resp2018, resp2017)
 
@@ -96,7 +90,7 @@ imd2022_query <- "select
                       d.imd19_decile_lsoas,
                       d.imd19_quintile_lsoas
                   from cpes.nc_2022_respondents@casref01 cpes
-                  join cpes.nc_2022_linkage_res_cas2403@casref01 ln on cpes.cpes_prn = ln.cpes_prn
+                  join cpes.nc_2022_linkage_res_cas2407@casref01 ln on cpes.cpes_prn = ln.cpes_prn
                   join av2021.at_tumour_england@casref01 at on ln.avtum_patientid = at.patientid and ln.avtum_tumourid = at.tumourid
                   left join av2021.at_geography_england@casref01 g on at.tumourid = g.tumourid
                   left join imd.imd2019_equal_lsoas@casref01 d on g.lsoa11_code = d.lsoa11_code
@@ -178,15 +172,15 @@ imd2017$datayear <- 2017
 imd_data <- bind_rows(imd2022, imd2021, imd2019, imd2018, imd2017)
 
 #checking for LSOA variation in different responses for the same tumour (linking IMD based on tumour not patient as looking at IMD at diag)
-imd_data_check <- imd_data %>%
-  group_by(TUMOURID) %>%
+imd_data_check <- imd_data |>
+  group_by(TUMOURID) |>
   dplyr::summarise(unique_lsoas = n_distinct(LSOA11_CODE))
 
-inconsistent_lsoa <- imd_data_check %>%
+inconsistent_lsoa <- imd_data_check |>
   filter(unique_lsoas > 1) #no inconsistent LSOAs by tumourid
 
-imd_data <- imd_data %>%
-  select(-c(PATIENTID, datayear)) %>%
+imd_data <- imd_data |>
+  select(-c(PATIENTID, datayear)) |>
   distinct()
 
 #link to resp_data table 
@@ -195,7 +189,7 @@ resp_data <- left_join(resp_data, imd_data, by = c("TUMOURID" = "TUMOURID"), rel
 
 ########### Converting variables to analysable formats ########### 
 #sexuality variable to binary
-resp_data <- resp_data %>% ####different question in different years
+resp_data <- resp_data |> ####different question in different years
   mutate(sexuality = case_when(datayear %in% c(2021,2022) & Q66 == 1 ~ "Heterosexual", 
                                datayear %in% c(2021,2022) & Q66 == 2 ~ "Gay or lesbian", 
                                datayear %in% c(2021,2022) & Q66 == 3 ~ "Bisexual", 
@@ -217,12 +211,12 @@ resp_data <- resp_data %>% ####different question in different years
                                datayear %in% c(2018, 2017) & Q65 == 5 ~ "Prefer not to say", 
                                datayear %in% c(2018, 2017) & Q65 == 6 ~ "Don't know/not sure", 
                                datayear %in% c(2018, 2017) & Q65 == 999 ~ "Missing", 
-                               TRUE ~ "Missing")) %>%
+                               TRUE ~ "Missing")) |>
   mutate(sexuality_bin = case_when(sexuality %in% c("Gay or lesbian", "Bisexual", "Other") ~ "Sexual minority", 
                                    sexuality == "Heterosexual" ~ "Heterosexual", 
                                    TRUE ~ "Missing")) 
 #language status variable to binary
-resp_data <- resp_data %>% ####different question in different years
+resp_data <- resp_data |> ####different question in different years
   mutate(lang_stat = case_when(datayear %in% c(2021,2022) & Q70 == 1 ~ "Native English speaker", 
                                datayear %in% c(2021,2022) & Q70 == 2 ~ "Non-native English speaker",
                                datayear == 2019 & Q71 == 1 ~ "Native English speaker", 
@@ -235,7 +229,7 @@ resp_data <- resp_data %>% ####different question in different years
 ########### Cleaning confounder data ########### 
 
 #note re checking other confoudners
-resp_data <- resp_data %>%
+resp_data <- resp_data |>
   mutate(ETHNICITY = ifelse(ETHNICITY == "0", NA, ETHNICITY))
 
 
